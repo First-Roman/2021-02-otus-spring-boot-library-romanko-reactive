cc50f233ad5ad42325234d6abae390ebea789996
First-Roman <79809612+First-Roman@users.noreply.github.com>
2021-05-26 00:40:47 +0300

    Delete src/main directory

--- src/main/java/ru/otus/library/repository/author/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.library.repository.author;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import ru.otus.library.models.Author;
-
-public interface AuthorRepository extends ReactiveMongoRepository<Author, String> {
-
-}
